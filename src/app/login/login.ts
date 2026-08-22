178dc47322c6824f03a83035230099ab841351dc
reboot0707 <bootcamp@contact.clam.tw>
2026-08-22 14:37:22 +0800

    feat: 儲存課堂登入 demo 進度

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-  title: string = '登入'
+  title: string = '登入畫面'
   role = 'admin'
+  accountPlaceHolder = '請輸入 go 帳號'; // camelCase
+  passwordPlaceHolder = '輸入通關密語...';
+  account = 'Ann'
+  password = ''
 
-  myTest() {
-    this.title += " Anon Tokyo"
+  myTest(para: PointerEvent) {
+    let a = confirm('Hello?');
+    console.log(a);
+    if (a) {
+      this.title = "登入中..."
+      console.log(`\{ id: ${this.account}, pwd: ${this.password} \}`);
+    } else {
+      this.title = '登入畫面'
+    }
+    // console.log(para);
   }
 }
