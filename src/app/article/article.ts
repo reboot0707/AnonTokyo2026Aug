36d98aa5df0892203615fa918bf621eae5478672
reboot0707 <bootcamp@contact.clam.tw>
2026-08-22 16:43:08 +0800

    save class status

--- src/app/article/article.ts
+++ src/app/article/article.ts
@@ -1,11 +1,31 @@
+import { NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-article',
-  imports: [],
+  imports: [NgClass, NgStyle],
   templateUrl: './article.html',
   styleUrl: './article.css',
 })
 export class Article {
+  fontSize = 10;
+  isRed = true;
+  isPopular = false;
 
+  myClick() {
+    this.isRed = !this.isRed;
+    console.log(this.isRed);
+  }
+
+  myFontClick() {
+    this.fontSize += 2;
+  }
+
+  popularArt() {
+    this.isPopular = true;
+  }
+
+  normalArt() {
+    this.isPopular = false;
+  }
 }
