8bbafeb0a0274c60511ad33650d970393ec28b84
reboot0707 <bootcamp@contact.clam.tw>
2026-08-22 15:59:21 +0800

    chore: add articla 空殼 for preparing practice

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from "./login/login";
 import { Practice1 } from './practice1/practice1';
+import { Article } from './article/article';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login, Practice1],
+  imports: [RouterOutlet, Login, Practice1, Article],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
