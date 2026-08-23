919e6a7b51b3638ae61a5e2ef1c69a8f25edf5e7
reboot0707 <bootcamp@contact.clam.tw>
2026-08-23 10:00:11 +0800

    feat: 練習 if else 迴圈操作頁面元素

--- src/app/article/article.ts
+++ src/app/article/article.ts
@@ -12,6 +12,93 @@ export class Article {
   isRed = true;
   isPopular = false;
 
+  popularArticleList = [
+    {
+      title: '熱門標題',
+      content: '熱門內容'
+    },
+    {
+      title: '熱門標題1',
+      content: '熱門內容1'
+    },
+    {
+      title: '熱門標題2',
+      content: '熱門內容2'
+    },
+    {
+      title: '熱門標題3',
+      content: '熱門內容3'
+    },
+    {
+      title: '熱門標題4',
+      content: '熱門內容4'
+    },
+    {
+      title: '熱門標題5',
+      content: '熱門內容5'
+    },
+    {
+      "title": "熱門標題6",
+      "content": "熱門內容6"
+    },
+    {
+      "title": "熱門標題7",
+      "content": "熱門內容7"
+    },
+    {
+      "title": "熱門標題8",
+      "content": "熱門內容8"
+    },
+    {
+      "title": "熱門標題9",
+      "content": "熱門內容9"
+    },
+    {
+      "title": "熱門標題10",
+      "content": "熱門內容10"
+    },
+    {
+      "title": "熱門標題11",
+      "content": "熱門內容11"
+    },
+    {
+      "title": "熱門標題12",
+      "content": "熱門內容12"
+    },
+    {
+      "title": "熱門標題13",
+      "content": "熱門內容13"
+    },
+    {
+      "title": "熱門標題14",
+      "content": "熱門內容14"
+    },
+    {
+      "title": "熱門標題15",
+      "content": "熱門內容15"
+    },
+    {
+      "title": "熱門標題16",
+      "content": "熱門內容16"
+    },
+    {
+      "title": "熱門標題17",
+      "content": "熱門內容17"
+    },
+    {
+      "title": "熱門標題18",
+      "content": "熱門內容18"
+    },
+    {
+      "title": "熱門標題19",
+      "content": "熱門內容19"
+    },
+    {
+      "title": "熱門標題20",
+      "content": "熱門內容20"
+    }
+  ]
+
   myClick() {
     this.isRed = !this.isRed;
     console.log(this.isRed);
@@ -27,5 +114,9 @@ export class Article {
 
   normalArt() {
     this.isPopular = false;
+  }
+
+  addArticle() {
+    this.popularArticleList.push({ title: 'a', content: 'a' });
   }
 }
