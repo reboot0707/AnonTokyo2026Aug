ea5030eb1c6ca93b9cd80c738aaf52461676c466
reboot0707 <bootcamp@contact.clam.tw>
2026-08-29 14:09:54 +0800

    fix: 我們需要用到 Zone, 所以把錯誤的 Zoneless 選項替換掉

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -8,7 +8,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideZonelessChangeDetection(),
+    provideZoneChangeDetection(),
     provideHttpClient()
   ]
 };
