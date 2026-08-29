9c2c0bc17905cd0f4f173e334331b6947f79bc20
reboot0707 <bootcamp@contact.clam.tw>
2026-08-29 16:21:06 +0800

    feat: 自訂 pipe 加參數

--- src/app/pipes/article-content-pipe.ts
+++ src/app/pipes/article-content-pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ArticleContentPipe implements PipeTransform {
 
-  transform(value: string): string {
-    return value.substring(0, 2) + '...';
+  transform(value: string, myformat: string = '...'): string {
+    return value.substring(0, 2) + myformat;
   }
 
 }
