7cc655e3c45350065535ab04d4d50c4044d8b925
reboot0707 <bootcamp@contact.clam.tw>
2026-08-30 10:29:05 +0800

    feat: 建立 Reactive Form 測試表單, 實作部分輸入資訊

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Login } from "./login/login";
 import { Practice1 } from './practice1/practice1';
 import { Article } from './article/article';
 import { Practice082901 } from "./practice082901/practice082901";
+import { SignUp } from "./sign-up/sign-up";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login, Practice1, Article, Practice082901],
+  imports: [RouterOutlet, Login, Practice1, Article, Practice082901, SignUp],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/sign-up/sign-up.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-up',
+  imports: [ReactiveFormsModule],
+  templateUrl: './sign-up.html',
+  styleUrl: './sign-up.css',
+})
+export class SignUp {
+  form = new FormGroup({
+    account: new FormControl('1'),
+    password: new FormControl('2'),
+    info: new FormGroup({
+      age: new FormControl(3),
+      tall: new FormControl(4)
+    }),
+    address: new FormArray([
+      new FormControl('5')
+    ])
+  })
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log(this.form);
+  }
+}
